2a86b09f26ccf6eefe49daa6842fc409822196d3
Jeremy Peters <info.peters@quantentunnel.de>
2020-04-02 16:28:27 +0200

    Make list more memory efficient

--- src/utils.c
+++ src/utils.c
@@ -2,18 +2,6 @@
 
 #include "utils.h"
 
-/* Lehmer random number generator */
-uint32_t rand32(uint32_t seed)
-{
-	seed += 0xe120fc15;
-	uint64_t tmp;
-	tmp = (uint64_t)seed * 0x4a39b70d;
-	uint32_t m1 = (uint32_t)((tmp >> 32) ^ tmp);
-	tmp = (uint64_t)m1 * 0x12fad5c9;
-	uint32_t m2 = (uint32_t)((tmp >> 32) ^ tmp);
-	return m2;
-}
-
 Color randomColor(void)
 {
 	return (Color) { randomRange(0.0, 1.0), randomRange(0.0, 1.0), randomRange(0.0, 1.0), 1.0 };
@@ -29,6 +17,18 @@ cpVect randomVector(int maxLenth)
 	return cpvmult(cpvforangle(randomRange(0.0, 2.0 * CP_PI)), rand32(rand()) % maxLenth);
 }
 
+/* Lehmer random number generator */
+uint32_t rand32(uint32_t seed)
+{
+	seed += 0xe120fc15;
+	uint64_t tmp;
+	tmp = (uint64_t)seed * 0x4a39b70d;
+	uint32_t m1 = (uint32_t)((tmp >> 32) ^ tmp);
+	tmp = (uint64_t)m1 * 0x12fad5c9;
+	uint32_t m2 = (uint32_t)((tmp >> 32) ^ tmp);
+	return m2;
+}
+
 CreatureList* NewList(void)
 {
 	return (CreatureList*)cpcalloc(1, sizeof(CreatureList));
@@ -36,7 +36,6 @@ CreatureList* NewList(void)
 
 void Add(CreatureList* list, Creature* creature)
 {
-	// todo: max counter to prevent unnecessary allocations when there are open slots at the end
 	list->count++;
 	list->list = (Creature**)cprealloc(list->list, list->count * sizeof(Creature*));
 	list->list[list->count - 1] = creature;
@@ -54,13 +53,24 @@ void Remove(CreatureList* list, Creature* creature, kill_cb kill)
 			/* Move last item to current position */
 			list->list[i] = list->list[list->count];
 
-			/* Remove reference */
-			list->list[list->count] = NULL;
-
 			/* Optional callback to free memory */
 			if (kill)
 			{
 				kill(creature);
+			}
+
+			/* Remove reference */
+			list->list[list->count] = NULL;
+
+			if (list->count == 0)
+			{
+				/* List is empty */
+				list->list = NULL;
+			}
+			else
+			{
+				/* Reduce allocation */
+				list->list = (Creature**)cprealloc(list->list, list->count * sizeof(Creature*));
 			}
 
 			return;
--- src/utils.h
+++ src/utils.h
@@ -9,12 +9,13 @@ cpFloat randomRange(cpFloat min, cpFloat max);
 cpVect randomVector(int maxLenth);
 uint32_t rand32(uint32_t seed);
 
-typedef void (*kill_cb)(Creature*);
-
+/* Dynamic array, not a linked list */
 typedef struct {
 	int count;
 	Creature** list;
 } CreatureList;
+
+typedef void (*kill_cb)(Creature*);
 
 CreatureList* NewList(void);
 void Add(CreatureList*, Creature*);
